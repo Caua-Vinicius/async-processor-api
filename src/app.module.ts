73058dadd1f336206dc342c1668ec29617449efa
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-07-04 15:36:48 -0300

    feat: configure app module with necessary imports for Jobs and Invoices

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
-
+import { JobsModule } from './jobs/jobs.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { InvoicesModule } from './invoices/invoices.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    JobsModule,
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    InvoicesModule,
+  ],
+
   controllers: [],
   providers: [],
 })
