cb5a834c5f5895225758401323f0fc92b42733f0
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-07-07 23:07:55 -0300

    refactor: update ServiceBusModule to use fully qualified namespace and DefaultAzureCredential for ServiceBusClient

--- src/service-bus/servicebus.module.ts
+++ src/service-bus/servicebus.module.ts
@@ -2,6 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { ServiceBusService } from './servicebus.service';
 import { ConfigService } from '@nestjs/config';
 import { ServiceBusClient } from '@azure/service-bus';
+import { DefaultAzureCredential } from '@azure/identity';
 
 @Global()
 @Module({
@@ -9,13 +10,19 @@ import { ServiceBusClient } from '@azure/service-bus';
     {
       provide: 'SERVICE_BUS_CLIENT',
       useFactory: (configService: ConfigService) => {
-        const connectionString = configService.get<string>(
-          'SERVICE_BUS_CONNECTION_STRING',
+        const fullyQualifiedNamespace = configService.get<string>(
+          'SERVICE_BUS_FULLY_QUALIFIED_NAMESPACE',
         );
-        if (!connectionString) {
-          throw new Error('SERVICE_BUS_CONNECTION_STRING it is not defined.');
+
+        if (!fullyQualifiedNamespace) {
+          throw new Error(
+            'SERVICE_BUS_FULLY_QUALIFIED_NAMESPACE it is not defined.',
+          );
         }
-        return new ServiceBusClient(connectionString);
+
+        const credential = new DefaultAzureCredential();
+
+        return new ServiceBusClient(fullyQualifiedNamespace, credential);
       },
       inject: [ConfigService],
     },
