c8f36a90a2096e4f9d8b556ff1a3bf2c35869a38
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-07-07 21:04:46 -0300

    feat: enhance JobsController to include Azure Blob Storage result retrieval

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { Job } from './jobs.schema';
 import { ValidateObjectIdPipe } from 'src/common/pipes/validate-object-id.pipe';
+import { Response } from 'express';
+import { AzureBlobService } from 'src/azure-blob/azure-blob.service';
 
 @Controller('jobs')
 export class JobsController {
-  constructor(private readonly jobsService: JobsService) {}
+  constructor(
+    private readonly jobsService: JobsService,
+    private readonly azureBlobService: AzureBlobService,
+  ) {}
 
   @Get(':id')
   async getJobById(
@@ -15,7 +20,7 @@ export class JobsController {
   }
 
   @Get(':id/status')
-  async getJobStatus(@Param('id') id: string) {
+  async getJobStatus(@Param('id', ValidateObjectIdPipe) id: string) {
     const job = await this.getJobById(id);
 
     return {
@@ -28,5 +33,44 @@ export class JobsController {
 
       ...(job.errorDetails && { error: job.errorDetails }),
     };
+  }
+
+  @Get(':id/result')
+  async getJobResult(
+    @Param('id', ValidateObjectIdPipe) id: string,
+    @Res() res: Response,
+  ) {
+    const job = await this.getJobById(id);
+
+    if (!job.result?.blobUrl) {
+      return res.status(404).json({
+        message: 'No result available for this job.',
+      });
+    }
+
+    const blobName = `invoice-${job.id}.pdf`;
+
+    try {
+      const { stream, length, type } =
+        await this.azureBlobService.getBlobStream(blobName);
+
+      res.setHeader('Content-Type', type);
+      res.setHeader('Content-Length', length);
+
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename="${blobName}"`,
+      );
+
+      // Set cache control headers to allow caching for 1 day
+      res.setHeader('Cache-Control', 'public, max-age=86400');
+
+      stream.pipe(res);
+    } catch (error) {
+      res.status(error.status || 500).send({
+        statusCode: error.status || 500,
+        message: error.message,
+      });
+    }
   }
 }
