e36c8dc68eb0b1f06f649725063c0dfc80fda64a
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-07-08 01:29:54 -0300

    refactor: switch database connection to use Azure Key Vault

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,40 @@
 import { Module } from '@nestjs/common';
 import { JobsModule } from './jobs/jobs.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { InvoicesModule } from './invoices/invoices.module';
+import { DefaultAzureCredential } from '@azure/identity';
+import { SecretClient } from '@azure/keyvault-secrets';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const keyVaultUrl = configService.get<string>('KEY_VAULT_URL');
+        if (!keyVaultUrl) {
+          throw new Error('KEY_VAULT_URL is not defined.');
+        }
+
+        const credential = new DefaultAzureCredential();
+        const secretClient = new SecretClient(keyVaultUrl, credential);
+
+        const secret = await secretClient.getSecret(
+          'COSMOS-DB-CONNECTION-STRING',
+        );
+
+        return {
+          uri: secret.value,
+        };
+      },
+      inject: [ConfigService],
+    }),
     JobsModule,
-    MongooseModule.forRoot(process.env.DATABASE_URL),
     InvoicesModule,
   ],
-
   controllers: [],
   providers: [],
 })
